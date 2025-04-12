87831b6b74e45c7c0cbdb1ae4ccd1b0e93e7190d
Felipe Brito <felipebrito.dev@gmail.com>
2025-04-12 20:57:09 -0300

    feat: add products table

---
+++ app/products/_components/table-columns.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Badge } from "@/app/_components/ui/badge";
+import { Product } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+
+const getStatusLabel = (status: string) => {
+  if (status === "IN_STOCK") {
+    return "Em estoque";
+  }
+  return "Fora de estoque";
+};
+
+export const productTableColumns: ColumnDef<Product>[] = [
+  {
+    accessorKey: "name",
+    header: "Produto",
+  },
+  {
+    accessorKey: "prece",
+    header: "Valor unitário",
+  },
+  {
+    accessorKey: "stock",
+    header: "Estoque",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: (row) => {
+      const product = row.row.original;
+      const label = getStatusLabel(product.status);
+      return (
+        <Badge variant={label === "Em estoque" ? "defaunt" : "outline"}>
+          {label}
+        </Badge>
+      );
+    },
+  },
+];
