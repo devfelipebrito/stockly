2db74dcc5bdaba0682617691bffdaa9c02cf2f3f
Felipe Brito <felipebrito.dev@gmail.com>
2025-04-19 23:12:15 -0300

    refactor: add route handlers

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -20,7 +20,10 @@ const ProductsPage = async () => {
           Novo Produto
         </Button>
       </div>
-      <DataTable columns={productTableColumns} data={products} />
+      <DataTable
+        columns={productTableColumns}
+        data={JSON.parse(JSON.stringify(products))}
+      />
     </div>
   );
 };
