8cf5c98d8373dfe4ffa6420d1f6296a9b6d7d072
Felipe Brito <felipebrito.dev@gmail.com>
2025-05-11 14:19:39 -0300

    feat: ensure product can be deleted

---
+++ app/products/_components/delete-dialog-content.tsx
@@ -0,0 +1,50 @@
+import { deleteProduct } from "@/app/_actions/product/delete-product";
+import {
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/app/_components/ui/alert-dialog";
+import { toast } from "sonner";
+
+interface DeleteProductDialogContentProps {
+  productId: string;
+}
+
+const DeleteProductDialogContent = ({
+  productId,
+}: DeleteProductDialogContentProps) => {
+  const handleContinueClick = async () => {
+    try {
+      await deleteProduct({ id: productId });
+      toast.success("Produto deletado com sucesso!");
+    } catch (error) {
+      console.error(error);
+      toast.error("Erro ao deletar o produto.");
+    }
+  };
+  return (
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>
+          Tem certeza que deseja deletar o produto?
+        </AlertDialogTitle>
+        <AlertDialogDescription>
+          Você esta prestes a deletar o produto. Esta ação não pode ser
+          desfeita. Deseja continuar?
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancelar</AlertDialogCancel>
+        <AlertDialogAction onClick={handleContinueClick}>
+          Continuar
+        </AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  );
+};
+
+export default DeleteProductDialogContent;
